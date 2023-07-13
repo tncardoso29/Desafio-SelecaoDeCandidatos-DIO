d442a1fbb5d685dd914d467369b520e6b3309296
Thiago Cardoso <tncardoso@outlook.com>
2023-07-13 12:01:57 -0300

    Iniciando o Projeto do Desafio

---
+++ src/br/cardoso/dio/desafios/candidatos/ProcessoSeletivo.java
@@ -0,0 +1,12 @@
+package br.cardoso.dio.desafios.candidatos;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(); // Quebrando as linhas...
+		System.out.println("..........: SISTEMA DE PROCESSOS SELETIVOS | v 1.0 :..........");
+		System.out.println(); // Quebrando as linhas...
+
+	}
+}
